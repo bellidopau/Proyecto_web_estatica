8ba08396e1fd19f46ae936576abee36aff472a0c
Milimmc <mili.mmc@gmail.com>
2024-05-15 17:39:46 -0300

    FORMULARIO CON LOCALSTORAGE TERMINADO♥🎉♥

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 
+
 let inputNombre = document.getElementById("nombre")
 let inputApellido = document.getElementById("apellido")
 let inputComentarios = document.getElementById("comentarios")
@@ -6,18 +7,19 @@ let inputEmail = document.getElementById("email")
 let inputTelefono = document.getElementById("telefono")
 let inputMotivo = document.getElementById("motivo")
 
-let persona2 = {
-    nombre: "milagros",
-    apellido: "cisneros",
-    comentarios: "holi",
-    email: "mili.mmc@gmail.com",
-    telefono: 1126439620,
-    motivo: "informacion",
+let persona = {
+    nombre: "",
+    apellido: "",
+    comentarios: "",
+    email: "",
+    telefono: "",
+    motivo: "",
 }
- 
-let persona = localStorage.getItem("persona")
+
+//rellenar formulario con localstorage
+
+persona = localStorage.getItem("persona")
 persona = JSON.parse(persona)
-console.log(persona)
 
 inputNombre.value = persona.nombre
 inputApellido.value = persona.apellido
@@ -26,11 +28,40 @@ inputEmail.value = persona.email
 inputTelefono.value = persona.telefono
 inputMotivo.value = persona.motivo
 
-function guardar_localstorage() {
-   localStorage.setItem("persona", JSON.stringify(persona2));
-}
+inputNombre.addEventListener("change", (evento) => {
+    persona.nombre = inputNombre.value 
+    guardar_localstorage() 
+    console.log(persona)
+})
+inputApellido.addEventListener("change", (evento) => {
+    persona.apellido = inputApellido.value 
+    guardar_localstorage() 
+    console.log(persona)
+})
+inputComentarios.addEventListener("change", (evento) => {
+    persona.comentarios = inputComentarios.value 
+    guardar_localstorage() 
+    console.log(persona)
+})
+inputEmail.addEventListener("change", (evento) => {
+    persona.email = inputEmail.value 
+    guardar_localstorage() 
+    console.log(persona)
+})
+inputTelefono.addEventListener("change", (evento) => {
+    persona.telefono = inputTelefono.value 
+    guardar_localstorage() 
+    console.log(persona)
+})
+inputMotivo.addEventListener("change", (evento) => {
+    persona.motivo = inputMotivo.value 
+    guardar_localstorage() 
+    console.log(persona)
+})
 
-//guardar_localstorage();
+function guardar_localstorage() {
+    localStorage.setItem("persona", JSON.stringify(persona));
+}
 
 
 
